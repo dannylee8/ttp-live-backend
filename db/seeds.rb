c6656c554d567b1ec89b522a12ff161f5c50e7c0
Danny Lee <dreamer@panix.com>
2020-02-17 15:26:55 -0500

    Fix seeds user ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@
 
   puts 1
 
-  mlee = User.create(
+  ml = User.create(
     username: 'ml', 
     email: 'ml@email.com', 
     password_digest: BCrypt::Password.create('password'),
@@ -55,28 +55,28 @@
     symbol: 'C',
     shares: 300,
     price: 75.79,
-    user_id: 1
+    user_id: danny.id
   )
 
   A1 = Stock.create(
     symbol: 'A',
     shares: 150,
     price: 82.82,
-    user_id: 1
+    user_id: danny.id
   )
 
   S1 = Stock.create(
     symbol: 'S',
     shares: 300,
     price: 7.70,
-    user_id: 1
+    user_id: danny.id
   )
 
   H1 = Stock.create(
     symbol: 'H',
     shares: 88,
     price: 87.94,
-    user_id: 1
+    user_id: danny.id
   )
 
   puts "User 1 stocks created."
@@ -87,28 +87,28 @@
     symbol: 'C',
     shares: 300,
     price: 78.79,
-    user_id: 2
+    user_id: ml.id
   )
 
   A2 = Stock.create(
     symbol: 'A',
     shares: 200,
     price: 55.82,
-    user_id: 2
+    user_id: ml.id
   )
 
   S2 = Stock.create(
     symbol: 'S',
     shares: 300,
     price: 10.70,
-    user_id: 2
+    user_id: ml.id
   )
 
   H2 = Stock.create(
     symbol: 'H',
     shares: 188,
     price: 8.79,
-    user_id: 2
+    user_id: ml.id
   )
 
   puts "User 2 stocks created."
@@ -119,28 +119,28 @@
     symbol: 'C',
     shares: 300,
     price: 58.79,
-    user_id: 3
+    user_id: rj.id
   )
 
   A3 = Stock.create(
     symbol: 'A',
     shares: 100,
     price: 40.82,
-    user_id: 3
+    user_id: rj.id
   )
 
   S3 = Stock.create(
     symbol: 'S',
     shares: 150,
     price: 10.70,
-    user_id: 3
+    user_id: rj.id
   )
 
   H3 = Stock.create(
     symbol: 'H',
     shares: 18,
     price: 42.20,
-    user_id: 3
+    user_id: rj.id
   )
 
   puts "User 3 stocks created."
@@ -151,28 +151,28 @@
       symbol: 'C',
       shares: 100,
       price: 58.79,
-      user_id: 4
+      user_id: jm.id
     )
   
     A4 = Stock.create(
       symbol: 'A',
       shares: 90,
       price: 40.82,
-      user_id: 4
+      user_id: jm.id
     )
   
     S4 = Stock.create(
       symbol: 'S',
       shares: 120,
       price: 10.70,
-      user_id: 4
+      user_id: jm.id
     )
-    
+
     H4 = Stock.create(
       symbol: 'H',
       shares: 40,
       price: 42.20,
-      user_id: 4
+      user_id: jm.id
     )
   
     puts "User 4 stocks created."
